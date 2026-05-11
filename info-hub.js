264f5138c3c4f450265d64b4606f21d6507af707
akworld666 <akagnihotri797473@gmail.com>
2026-05-12 03:17:32 +0530

    debug: add request logger to info hub

--- info-hub.js
+++ info-hub.js
@@ -13,6 +13,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// DEBUG LOGGER
+app.use((req, res, next) => {
+  console.log(`[INFO-HUB] ${req.method} ${req.url}`);
+  next();
+});
+
 // Clerk Middleware
 app.use(clerkMiddleware({
   publishableKey: process.env.CLERK_PUBLISHABLE_KEY,
